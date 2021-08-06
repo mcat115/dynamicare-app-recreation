02acc13adf1d9f9dab131daf8903dc56645bbaae
Michael Catanese <michaeljcatanese@gmail.com>
2021-08-06 16:23:04 -0400

    nav component seperated, color cues removed

--- components/Home.js
+++ components/Home.js
@@ -1,80 +1,20 @@
 import React from "react"
 import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native"
+import NavBar from "./NavBar"
 
-const Home = ({ navigation }) => {
+const Home = () => {
   return (
     <>
       <View style={styles.homeContainer}>
         <Text>This is the home page placeholder!! Amazing stuff!</Text>
         <Image source={require("../assets/logo.png")} style={styles.image} />
       </View>
-      <View style={styles.nav}>
-        <TouchableOpacity
-          onPress={() => navigation.replace("Home")}
-          style={styles.button}
-        >
-          <Image
-            source={require("../assets/home.png")}
-            style={styles.coloredButtonImage}
-          />
-          <Text style={{ color: "blue" }}>Home</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => navigation.replace("History")}
-          style={styles.button}
-        >
-          <Image
-            source={require("../assets/clock.png")}
-            style={styles.marginButtonImage}
-          />
-          <Text>History</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => navigation.replace("Settings")}
-          style={styles.button}
-        >
-          <Image
-            source={require("../assets/settings.png")}
-            style={styles.marginButtonImage}
-          />
-          <Text>Settings</Text>
-        </TouchableOpacity>
-      </View>
+      <NavBar />
     </>
   )
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: "white",
-    marginHorizontal: 20,
-  },
-  nav: {
-    backgroundColor: "white",
-    flexDirection: "row",
-    justifyContent: "center",
-    marginTop: "auto",
-  },
-  marginButtonImage: {
-    padding: 10,
-    margin: 5,
-    height: 25,
-    width: 25,
-    resizeMode: "stretch",
-    marginLeft: 10,
-  },
-  coloredButtonImage: {
-    padding: 10,
-    margin: 5,
-    height: 25,
-    width: 25,
-    resizeMode: "stretch",
-    tintColor: "blue",
-  },
-  button: {
-    marginLeft: 30,
-    marginRight: 30,
-  },
   image: {
     padding: 10,
     margin: 5,
--- components/Settings.js
+++ components/Settings.js
@@ -1,15 +1,9 @@
 import React from "react"
-import {
-  StyleSheet,
-  Text,
-  ScrollView,
-  TouchableOpacity,
-  View,
-  Image,
-} from "react-native"
+import { StyleSheet, Text, ScrollView } from "react-native"
 import SettingsButton from "./SettingsButton"
+import NavBar from "./NavBar"
 
-const Settings = ({ navigation }) => {
+const Settings = () => {
   return (
     <>
       <ScrollView style={styles.scrollView}>
@@ -31,38 +25,7 @@ const Settings = ({ navigation }) => {
         <Text style={styles.footnotes}>You are using version 123.456</Text>
         <Text style={styles.footnotes}>We appreciate you!</Text>
       </ScrollView>
-      <View style={styles.nav}>
-        <TouchableOpacity
-          onPress={() => navigation.replace("Home")}
-          style={styles.button}
-        >
-          <Image
-            source={require("../assets/home.png")}
-            style={styles.buttonImage}
-          />
-          <Text>Home</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => navigation.replace("History")}
-          style={styles.button}
-        >
-          <Image
-            source={require("../assets/clock.png")}
-            style={styles.marginButtonImage}
-          />
-          <Text>History</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => navigation.replace("Settings")}
-          style={styles.button}
-        >
-          <Image
-            source={require("../assets/settings.png")}
-            style={styles.coloredButtonImage}
-          />
-          <Text style={{ color: "blue" }}>Settings</Text>
-        </TouchableOpacity>
-      </View>
+      <NavBar />
     </>
   )
 }
@@ -79,39 +42,5 @@ const styles = StyleSheet.create({
   },
   footnotes: {
     textAlign: "center",
-  },
-  nav: {
-    backgroundColor: "white",
-    flexDirection: "row",
-    justifyContent: "center",
-    marginTop: "auto",
-  },
-  buttonImage: {
-    padding: 10,
-    margin: 5,
-    height: 25,
-    width: 25,
-    resizeMode: "stretch",
-  },
-  marginButtonImage: {
-    padding: 10,
-    margin: 5,
-    height: 25,
-    width: 25,
-    resizeMode: "stretch",
-    marginLeft: 10,
-  },
-  coloredButtonImage: {
-    padding: 10,
-    margin: 5,
-    height: 25,
-    width: 25,
-    resizeMode: "stretch",
-    tintColor: "blue",
-    marginLeft: 10,
-  },
-  button: {
-    marginLeft: 30,
-    marginRight: 30,
   },
 })
